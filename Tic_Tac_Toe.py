0eb784f338bb3bc8e7a05cbbc5bb52221aeb7109
yingkexu <36081133+yingkexu@users.noreply.github.com>
2018-05-03 19:44:13 +0100

    A leaked look at tic tac toe!!!

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,4 +1,17 @@
 import random
 
 def drawBoard(board):
-    print(board[7] + '|' 
+    print(board[7] + '|' + board[8] + '|' +board[9])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[1]+ '|' + board[2] + '|' + board[3])
+
+
+def inputPlayerLetter():
+    letter = ' '
+    while not(letter == 'X' or letter == 'O'):
+        print('Do you want to be X or O?')
+        letter = input().upper()
+        
+
