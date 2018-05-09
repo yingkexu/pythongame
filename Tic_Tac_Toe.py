8c3b29bd38ffbf0e54def5a42487b8967f5a050d
yingkexu <36081133+yingkexu@users.noreply.github.com>
2018-05-09 19:04:05 +0100

    update

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -5,7 +5,7 @@ def drawBoard(board):
     print('-+-+-')
     print(board[4] + '|' + board[5] + '|' + board[6])
     print('-+-+-')
-    print(board[1]+ '|' + board[2] + '|' + board[3])
+    print(board[1] + '|' + board[2] + '|' + board[3])
 
 
 def inputPlayerLetter():
@@ -13,5 +13,18 @@ def inputPlayerLetter():
     while not(letter == 'X' or letter == 'O'):
         print('Do you want to be X or O?')
         letter = input().upper()
-        
+        if letter == 'X':
+            return ['X','O']
+        else: 
+            return['O','X']        
+               
+def whoGoesForst():
+    if random.randint(0,1) == 0:
+        return 'computer'
+    else:
+        return'player'
+
+def makeMove(board, letter, move):
+    board[move] = letter
+
 
