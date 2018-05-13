baf90774557cc707a88aafde5541e9d55f597e93
yingkexu <36081133+yingkexu@users.noreply.github.com>
2018-05-13 10:40:47 +0100

    Just a save! We are so close!

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -27,4 +27,68 @@ def whoGoesForst():
 def makeMove(board, letter, move):
     board[move] = letter
 
+def isWinner(bo, le):
+    return((bo[7] == le and bo[8] == le and bo[9] == le) or
+           (bo[4] == le and bo[5] == le and bo[6] == le) or
+           (bo[1] == le and bo[2] == le and bo[3] == le) or
+           (bo[7] == le and bo[4] == le and bo[1] == le) or
+           (bo[8] == le and bo[5] == le and bo[2] == le) or
+           (bo[9] == le and bo[6] == le and bo[3] == le) or
+           (bo[7] == le and bo[5] == le and bo[3] == le) or
+           (bo[9] == le and bo[6] == le and bo[1] == le))
 
+def getBoardCopy(board):
+    boardCopy = [ ]
+    for i in board:
+        boardCopy.append(i)
+    return BoardCopy
+
+def isSpaceFree(board, move):
+    return board[move] == ' '
+    
+def getPlayerMove(board):
+    move = ' '
+    while move not in '1 2 3 4 5 6 7 8 9 ' .split() or isSpaceFree(board, int(move)):
+        print('What is your next move? (1-9) choose a place that has not been taken.')
+        move = input()
+    return int(move)
+
+def chooseRandomMoveFromList(board, movesList):
+    possibleMoves = [ ]
+    for i in movesList:
+        if isSpaceFree(board, i):
+            possibleMoves.append(i)
+
+    if len(possibleMoves) != 0:
+        return random.choice(possibleMoves)
+    else:
+        return None
+
+def getComputerMove(board, computerLetter):
+    if computerLetter == 'X':
+        playerLetter = 'O'
+    else:
+        playerLetter = 'X'
+    
+    for i in range(1, 10):
+        boardCopy = getBoardCopy(board)
+        if isSpaceFree(boardCopy, i):
+            makeMove(boardCopy, computerLetter, i)
+        if ifWinner(boardCopy, computerLetter):
+            return i
+
+    for i in range(1, 10):
+        boardCopy = getBoardCopy(board)
+        if isSpaceFree(boardCopy, i):
+            makeMove(boardCopy, playerLetter, i)
+            if isWinner(boardCopy, playerLetter):
+                return i
+            
+    move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
+    if move != None:
+        return move
+
+    if isSpaceFree(board, 5):
+        return 5
+    
+    
